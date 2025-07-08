db52eab6643d471f58c7a6d0607d221721e5efe5
Vasil Panchev <vasepanchev@gmail.com>
2025-07-08 19:48:11 +0200

    Refactor into a class

--- link_shortener.py
+++ link_shortener.py
@@ -1,76 +1,90 @@
 import json
 import uuid
 import re
-from typing import Optional, Dict
+from typing import Dict
+from filelock import FileLock
 
 
 class Config:
     """A class for application configuration constants."""
+    LINKS_PATH = "links.json"
     SHORT_DOMAIN = "https://shortlinkdomain.com/"
-    LINK_PATTERN = re.compile(
-        r"^(https?://)?(www\.)?([a-zA-Z0-9]([a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z]{2,24}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)$"
-    )
+    DOMAIN_PATTERN = r"([a-zA-Z0-9]([a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z]{2,24}"
+    LINK_PATTERN = re.compile(rf"^(https?://)?(www\.)?{DOMAIN_PATTERN}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)$")
 
 
-def validate_link(link: str) -> bool:
-    """Validates a URL format (with http/https, optional www, and valid domain structure)."""
-    return bool(Config.LINK_PATTERN.match(link))
+class LinkShortener:
+    def __init__(self, link: str):
+        self.link = link
 
+    def validate_link(self) -> bool:
+        """Validates a URL format (with http/https, optional www, and valid domain structure)."""
+        return bool(Config.LINK_PATTERN.match(self.link))
 
-def generate_unique_link_id() -> str:
-    """Generates a short, URL unique ID (8 chars)."""
-    try:
-        with open("links.json", "r") as f:
-            data = json.load(f)
-    except FileNotFoundError:
-        data = {}
-
-    existing_ids = set(data.keys())
-    unique_link_id = uuid.uuid4().hex[:8]
-    while unique_link_id in existing_ids:
+    @staticmethod
+    def generate_unique_link_id(existing_ids: set) -> str:
+        """Generate a short, URL unique ID (8 chars)."""
         unique_link_id = uuid.uuid4().hex[:8]
-    return unique_link_id
+        while unique_link_id in existing_ids:
+            unique_link_id = uuid.uuid4().hex[:8]
+        return unique_link_id
 
+    @staticmethod
+    def read_json_file(file: str) -> Dict[str, str]:
+        """Read JSON file with lock."""
+        try:
+            lock_path = f"{file}.lock"
 
-def read_json_file(file: str) -> Dict[str, str]:
-    try:
-        with open(file, "r") as f:
-            data = json.load(f)
-    except FileNotFoundError:
-        data = {}
+            lock = FileLock(lock_path)
+            with lock.acquire(timeout=10):
+                with open(file, "r") as f:
+                    data = json.load(f)
+        except FileNotFoundError:
+            data = {}
+        except TimeoutError:
+            raise RuntimeError("Could not acquire lock.")
 
-    return data
+        return data
 
+    @staticmethod
+    def write_to_json_file(file: str, data: Dict[str, str]):
+        """Write to JSON file with lock"""
+        lock_path = f"{file}.lock"
 
-def write_to_json_file(file: str, data: Dict[str, str]):
-    with open(file, "w") as f:
-        json.dump(data, f, indent=4)
+        lock = FileLock(lock_path)
+        try:
+            with lock.acquire(timeout=10):
+                with open(file, "w") as f:
+                    json.dump(data, f, indent=4)
+        except TimeoutError:
+            raise "Could not acquire lock."
 
+    def generate_shortened_link(self) -> str:
+        """Generates a shortened link if the input is valid, else returns None."""
+        if not self.validate_link():
+            return "The provided link is not valid."
+        try:
+            data = self.read_json_file(Config.LINKS_PATH)
+            existing_ids = set(data.keys())
 
-def generate_shortened_link(link: str) -> str:
-    """Generates a shortened link if the input is valid, else returns None."""
-    if not validate_link(link):
-        return "The provided link is not valid."
-    try:
-        data = read_json_file("links.json")
+            shortened_id = self.generate_unique_link_id(existing_ids)
+            data[shortened_id] = self.link if self.link.startswith(("https://", "http://")) else f"https://{self.link}"
 
-        shortened_id = generate_unique_link_id()
-        data[shortened_id] = link
+            self.write_to_json_file(Config.LINKS_PATH, data)
 
-        write_to_json_file("links.json", data)
+            result = f"Successfully generated shortened link: '{Config.SHORT_DOMAIN}{shortened_id}'"
 
-        result = f"Successfully generated shortened link: '{Config.SHORT_DOMAIN}{shortened_id}'"
+        except (TimeoutError, FileNotFoundError):
+            result = "Shortened link couldn't be generated."
 
-    except Exception:
-        result = "Shortened link couldn't be generated."
-
-    return result
+        return result
 
 
 def main():
     print("Enter a URL to shorten (e.g., 'https://example.com'):")
     link = input().strip().rstrip('/')
-    print(generate_shortened_link(link))
+    link_shortener = LinkShortener(link)
+    print(link_shortener.generate_shortened_link())
 
 
 if __name__ == "__main__":
